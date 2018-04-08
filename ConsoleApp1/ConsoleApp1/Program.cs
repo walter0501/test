e6e8ebeab7a67b2d50ea40b12a0166c46f73b4dd
walter0501 <walter0501@gmail.com>
2018-04-08 23:59:57 +0800

    fib console app

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Fib(60);
+            Console.WriteLine(a);
+        }
+
+        static int Fib(int a)
+        {
+            Func<int, int, int,int> FibIter = null;
+            FibIter = (x, y, z) => {
+                if (z == 0) return y;
+                else
+                {
+                    return FibIter(x + y, x, z - 1);
+                }
+                 };
+            return FibIter(1, 0, a);
+        }
+    }
+}
